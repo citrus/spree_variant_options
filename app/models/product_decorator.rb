6267cca874fea75e2853e724b106855057d6084e
Spencer Steffen <spencer@citrusme.com>
2011-06-10 04:34:04 -0700

    move variant_options_hash to model rather than helper

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -7,5 +7,18 @@ Product.class_eval do
   def grouped_option_values
     option_values.group_by(&:option_type)
   end
-
+  
+  def variant_options_hash  
+    return @variant_options_hash if @variant_options_hash
+    @variant_options_hash = Hash[grouped_option_values.map{ |type, values| 
+      [type.id.inspect, Hash[values.map{ |value|         
+        [value.id.inspect, Hash[variants.includes(:option_values).select{ |variant| 
+          variant.option_values.select{ |val| 
+            val.id == value.id && val.option_type_id == type.id 
+          }.length == 1 }.map{ |v| [ v.id, v.count_on_hand ] }]
+        ]
+      }]]
+    }]
+  end
+  
 end
