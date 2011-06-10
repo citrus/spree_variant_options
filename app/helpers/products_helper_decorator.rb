617caa1150c862fd3ce2505292cf3c167f372256
Spencer Steffen <spencer@citrusme.com>
2011-06-10 03:40:48 -0700

    dialing in the javascript

--- app/helpers/products_helper_decorator.rb
+++ app/helpers/products_helper_decorator.rb
@@ -4,10 +4,10 @@ ProductsHelper.class_eval do
     return unless @product
     Hash[@product.grouped_option_values.map{ |type, values| 
       [type.id.inspect, Hash[values.map{ |value|         
-        [value.id.inspect, @variants.select{ |variant| 
+        [value.id.inspect, Hash[@variants.select{ |variant| 
           variant.option_values.select{ |val| 
             val.id == value.id && val.option_type_id == type.id 
-          }.length == 1 }.map{ |v| v.id }
+          }.length == 1 }.map{ |v| [ v.id, v.count_on_hand ] }]
         ]
       }]]
     }].to_json.html_safe
--- public/javascripts/variant_options.js
+++ public/javascripts/variant_options.js
@@ -1,30 +1,63 @@
 function VariantOptions(options) {
+  
   var options = options;
+  var div, types, values, buttons, index = -1, selected = [];
   
   function init() {
-    this.div = $('#product-variants');
-    console.log(this.div);
-    
-    this.types = this.div.find('.variant-option-type');
-    this.values = this.div.find('.variant-option-values');
-    
-    //this.div.find('a.option-value').click(handle_click);
-    
+    div = $('#product-variants');
+    types = div.find('.variant-option-type');
+    values = div.find('.variant-option-values');
+    disable(values.find('a.option-value'));
+    advance();
+  }
+  
+  function disable(btns) {
+    return btns.removeClass('selected').fadeTo(0, 0.3).click(cancel_click);
+  }
+  
+  function enable(btns) {
+    return btns.fadeTo(0, 1).unbind('click').click(handle_click);
+  }
+  
+  function advance() {
+    index++;
+    enable(set_buttons());
+  }
+  
+  function set_buttons() {
+    return buttons = $(values[index]).find('a.option-value');
+  }
+  
+  function cancel_click(evt) {
+    evt.preventDefault();
   }
   
   function handle_click(evt) {
     evt.preventDefault();
-    try {
-      var ids = this.rel.split("-");
-      var vids = options[ids[0]][ids[1]];   
-    } catch(error) {
-      console.log("Type / Value combination could not be found");
-      return false;
+    var ids = this.rel.split("-");
+    
+    disable(buttons.not(this).removeClass('selected'));
+    var a = enable($(this).addClass('selected'));
+    
+    var parent_index = parseInt($("#option_type_" + ids[0]).attr('class').replace('index-', ''));    
+    if (parent_index < index) {
+      index = parent_index;      
+      selected = selected.slice(0, index);
+      set_buttons();
+      disable(buttons.not(this));
     }
     
-    console.log(vids);
-    
+    try {
+      var vids = options[ids[0]][ids[1]];   
+      selected[index] = this.rel;
+      advance();
+      console.log("SEL:", selected.length, selected);
+      
+    } catch(error) {
+      console.log("Type / Value " + this.rel + " combination could not be found");
+    }
   }
   
   $(document).ready(init);
+  
 };
