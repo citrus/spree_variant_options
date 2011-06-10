dd94ac37bb8ee54d479a1b073781c81d80cf8140
Spencer Steffen <spencer@citrusme.com>
2011-06-10 04:20:22 -0700

    variants found!

--- public/javascripts/variant_options.js
+++ public/javascripts/variant_options.js
@@ -1,7 +1,14 @@
+if (!Object.keys) Object.keys = function(o) {
+  if (o !== Object(o)) throw new TypeError('Object.keys called on non-object');
+  var ret=[],p;
+  for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+  return ret;
+}
+
 function VariantOptions(options) {
   
   var options = options;
-  var div, types, values, buttons, index = -1, selected = [];
+  var variant, div, types, values, buttons, index = -1, selected = [];
   
   function init() {
     div = $('#product-variants');
@@ -24,8 +31,40 @@ function VariantOptions(options) {
     enable(set_buttons());
   }
   
+  function out_of_stock() {
+    alert('out of stock!');
+  }  
+  
+  function select() {
+    console.log('choose variant');
+  }
+  
+  
+  
   function set_buttons() {
     return buttons = $(values[index]).find('a.option-value');
+  }
+  
+  function find_variant() {
+    console.log('find_variant');
+    console.log(options);
+    
+    var res = [], ids = [], i = selected.length
+    while(i--) {
+      ids = ids.concat(Object.keys(selected[i]));
+    }
+    ids = ids.sort();
+    i = ids.length
+    while(i--) {
+      if (ids[i - 1] == ids[i]) {
+        res.push(ids[i]);
+      }
+    }
+    if (res.length != 1) {
+      return false;
+    }
+    variant = res[0];
+    return selected[0][variant];
   }
   
   function cancel_click(evt) {
@@ -48,11 +87,17 @@ function VariantOptions(options) {
     }
     
     try {
-      var vids = options[ids[0]][ids[1]];   
-      selected[index] = this.rel;
-      advance();
-      console.log("SEL:", selected.length, selected);
-      
+      var inv, vids = options[ids[0]][ids[1]];   
+      selected[index] = vids;
+      inv = find_variant();
+      console.log(variant, inv);
+      if (inv === false) {
+        advance();      
+      } else if (inv == 0) {
+        out_of_stock();
+      } else {
+        select();
+      }
     } catch(error) {
       console.log("Type / Value " + this.rel + " combination could not be found");
     }
