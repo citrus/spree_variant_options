738d5924c62bca6cc05b94347f3111119bb02510
Spencer Steffen <spencer@citrusme.com>
2011-06-10 15:09:13 -0700

    adding inventory features and steps

--- features/step_definitions/variant_options.rb
+++ features/step_definitions/variant_options.rb
@@ -2,6 +2,14 @@ Given /^I have a product( with variants)?$/ do |has_variants|
   @product = Factory.create(has_variants ? :product_with_variants : :product)
 end
 
+Given /^the "([^"]*)" variant is out of stock$/ do |descriptor|
+  flunk unless @product
+  values = descriptor.split(" ")
+  values.map! { |word| OptionValue.find_by_presentation(word) }
+  variant = @product.variants.includes(:option_values).select{|i| i.option_value_ids.sort == values.map(&:id) }.first
+  @variant = variant if variant.update_attributes(:count_on_hand => 0)
+end
+
 Then /^the source should contain the options hash$/ do
   assert source.include?("VariantOptions(#{@product.variant_options_hash.to_json})")
 end
@@ -37,4 +45,14 @@ Then /^the add to cart button should be (enabled|disabled)?$/ do |state|
   enabled = state == "enabled"
   button = find("#cart-form button[type=submit]")
   assert_equal !enabled, button.native.attribute("disabled") == "true"
+end
+
+Then /^I should see an (out-of|in)-stock link for "([^"]*)"$/ do |state, button|
+  in_stock = state == "in"
+  buttons = button.split(", ")
+  buttons.each do |button|
+    link = find("a.option-value.#{in_stock ? 'in-stock' : 'out-of-stock'}")
+    assert_not_nil link
+    assert link.native.attribute("class").include?("enabled")
+  end
 end
--- features/variant_options.feature
+++ features/variant_options.feature
@@ -10,7 +10,8 @@ Feature: Products should have variant options
     And I should see disabled links for the second option type
     And I should have a hidden input for the selected variant
     And the add to cart button should be disabled
-    
+  
+  
   Scenario: Interact with options for a product
     Given I have a product with variants
     And I'm on the product page for the first product
@@ -24,4 +25,12 @@ Feature: Products should have variant options
     And the add to cart button should be disabled
     When I follow "Red" within the second set of options
     And the add to cart button should be enabled
+      
+  Scenario: Should show out of stock for appropriate variants
+    Given I have a product with variants
+    And the "Small Green" variant is out of stock
+    And I'm on the product page for the first product
+    When I follow "Small" within the first set of options
+    Then I should see an out-of-stock link for "Green"
+    And I should see an in-stock link for "Red, Blue, Black, White, Gray"
     
