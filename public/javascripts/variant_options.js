70bd6383e7ca08584cf53ae621979196c3220dc0
Spencer Steffen <spencer@citrusme.com>
2011-06-10 21:20:54 -0700

    getting the out of stock stuff back

--- public/javascripts/variant_options.js
+++ public/javascripts/variant_options.js
@@ -1,7 +1,29 @@
+if (!Object.keys) Object.keys = function(o) {
+  if (o !== Object(o)) throw new TypeError('Object.keys called on non-object');
+  var ret=[],p;
+  for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+  return ret;
+}
+if (!Array.find_matches) Array.find_matches = function(a) {
+  var i, m = [];
+  a = a.sort();
+  i = a.length
+  while(i--) {
+    if (a[i - 1] == a[i]) {
+      m.push(a[i]);
+    }
+  }
+  if (m.length == 0) {
+    return false;
+  }
+  return m;
+}
+
+
 function VariantOptions(options) {
   
   var options = options;
-  var divs, parent, index = 0;
+  var variant, divs, parent, index = 0;
   
   function init() {
     divs = $('#product-variants .variant-options'); 
@@ -32,13 +54,44 @@ function VariantOptions(options) {
     enable(buttons);
   }
   
+  function find_variant() {
+    var selected = divs.find('a.selected');
+    if (selected.length != divs.length) { return };
+    var _var, ids, matches, variants, variant_ids = [];
+    selected.each(function(i, element) {
+      try {
+        ids = element.rel.split('-')
+        _var = options[ids[0]][ids[1]];
+        variants = _var;
+        variant_ids = variant_ids.concat(Object.keys(_var));
+      } catch(error) {
+        alert(error);
+      }
+    });
+    matches = Array.find_matches(variant_ids);
+    if (matches.length == 1) { 
+      variant = variants[matches[0]];
+      console.log(variant);
+      if (variant.count == 0) alert('out of stock');
+    }
+  }
+  
+  function toggle() {
+    if (variant) {
+      $('#variant_id').val(variant);
+      $('button[type=submit]').attr('disabled', false).fadeTo(0, 1);
+    } else {
+      $('#variant_id').val('');
+      $('button[type=submit]').attr('disabled', true).fadeTo(0, 0.5);
+    }    
+  }
+  
   function get_index(parent) {
     return parseInt($(parent).attr('class').replace(/[^\d]/g, ''));
   }
   
-  function handle_clear(evt) {
-    evt.preventDefault();
-    update(get_index(this));
+  function clear(i) {
+    update(i);
     enable(buttons.removeClass('selected'));
     parent.nextAll().each(function(index, element) {
       disable($(element).find('a.option-value').addClass('locked').unbind('click'));
@@ -46,16 +99,25 @@ function VariantOptions(options) {
     });
   }
   
+  function handle_clear(evt) {
+    evt.preventDefault();
+    clear(get_index(this));
+  }
+  
   function handle_click(evt) {
     evt.preventDefault();
     var a = $(this);
     if (!parent.has(a).length) {
-      update( divs.index(a.parents('.variant-options:first')) ); 
+      clear( divs.index(a.parents('.variant-options:first')) ); 
     }
     disable(buttons);
     var a = enable(a.addClass('selected'));
     parent.find('a.clear-button').css('display', 'block');
-    advance();    
+    advance();
+    if (find_variant()) {
+      console.log('VARIANT!');
+      
+    };
   }
   
   $(document).ready(init);
