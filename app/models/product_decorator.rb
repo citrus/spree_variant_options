e51dd3db364bf6cec207dca0f4386bd6ea4a9262
Spencer Steffen <spencer@citrusme.com>
2011-06-14 00:52:50 -0700

    make price currency formatted

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,5 +1,7 @@
 Product.class_eval do
 
+  include ActionView::Helpers::NumberHelper
+
   def option_values
     option_types.map{|i| i.option_values }.flatten.uniq
   end
@@ -15,7 +17,7 @@ Product.class_eval do
         [value.id.inspect, Hash[variants.includes(:option_values).select{ |variant| 
           variant.option_values.select{ |val| 
             val.id == value.id && val.option_type_id == type.id 
-          }.length == 1 }.map{ |v| [ v.id, { :id => v.id, :count => v.count_on_hand, :price => v.price } ] }]
+          }.length == 1 }.map{ |v| [ v.id, { :id => v.id, :count => v.count_on_hand, :price => number_to_currency(v.price) } ] }]
         ]
       }]]
     }]
