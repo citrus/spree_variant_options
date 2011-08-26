449a1b8dd471c8f71f2e03ed6c3e4c2a1978b713
Fabien Franzen <info@atelierfabien.be>
2011-08-26 21:06:27 +0200

    Don't display unavailable options at all

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -3,25 +3,35 @@ Product.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def option_values
-    option_types.map{|i| i.option_values }.flatten.uniq
+    option_types.map { |i| i.option_values }.flatten.uniq
   end
   
   def grouped_option_values
     option_values.group_by(&:option_type)
   end
+
+  def available_variant_options
+    return @available_variant_options if @available_variant_options
+    @available_variant_options = variants.includes(:option_values).map { |variant| 
+      variant.option_values
+    }.flatten.uniq
+  end
+  
+  def variants_for_option_value(value)
+    variants.includes(:option_values).select { |i| i.option_values.select { |j| j == value }.length == 1 }
+  end
   
   def variant_options_hash  
     return @variant_options_hash if @variant_options_hash
-    @variant_options_hash = Hash[grouped_option_values.map{ |type, values| 
-      [type.id.inspect, Hash[values.map{ |value|         
-        [value.id.inspect, Hash[variants.includes(:option_values).select{ |variant| 
-          variant.option_values.select{ |val| 
+    @variant_options_hash = Hash[grouped_option_values.map { |type, values| 
+      [type.id.inspect, Hash[values.map { |value|     
+        hsh = Hash[variants.includes(:option_values).select { |variant| 
+          variant.option_values.select { |val| 
             val.id == value.id && val.option_type_id == type.id 
-          }.length == 1 }.map{ |v| [ v.id, v.variant_options_hash ] }]
-        ]
+          }.length == 1 }.map { |v| [ v.id, v.variant_options_hash ] }]
+        [value.id.inspect, hsh]
       }]]
     }]
-    @variant_options_hash
   end
   
 end
