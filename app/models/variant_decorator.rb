ab33b64c669a6d0f57e4a615db181e4c873204c0
Fabien Franzen <info@atelierfabien.be>
2011-11-17 20:54:15 +0100

    Be VAT aware

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -3,10 +3,12 @@ Variant.class_eval do
   include ActionView::Helpers::NumberHelper
   
   def variant_options_hash
+    actual_price  = self.price
+    actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     { 
       :id => self.id, 
       :count => self.count_on_hand, 
-      :price => number_to_currency(self.price)
+      :price => number_to_currency(actual_price)
     }
   end
     
