53d6c972dd8503afc67a96aeb4ab2b9ee8e3c1e1
Fabien Franzen <info@atelierfabien.be>
2011-11-17 22:01:08 +0100

    Implemented variant_options_hash in a much cleaner way - more efficient too

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -3,35 +3,35 @@ Product.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def option_values
-    option_types.map { |i| i.option_values }.flatten.uniq
+    @_option_values ||= option_types.map { |i| i.option_values }.flatten.uniq
   end
   
   def grouped_option_values
-    option_values.group_by(&:option_type)
+    @_grouped_option_values ||= option_values.group_by(&:option_type)
   end
 
   def available_variant_options
-    return @available_variant_options if @available_variant_options
-    @available_variant_options = variants.includes(:option_values).map { |variant| 
+    @_available_variant_options ||= variants.includes(:option_values).map { |variant| 
       variant.option_values
     }.flatten.uniq
   end
   
-  def variants_for_option_value(value)
-    variants.includes(:option_values).select { |i| i.option_values.select { |j| j == value }.length == 1 }
+  def variants_for_option_value(value)    
+    @_variant_option_values ||= variants.includes(:option_values).all
+    @_variant_option_values.select { |i| i.option_values.select { |j| j == value }.length == 1 }
   end
   
-  def variant_options_hash  
-    return @variant_options_hash if @variant_options_hash
-    @variant_options_hash = Hash[grouped_option_values.map { |type, values| 
-      [type.id.inspect, Hash[values.map { |value|     
-        hsh = Hash[variants.includes(:option_values).select { |variant| 
-          variant.option_values.select { |val| 
-            val.id == value.id && val.option_type_id == type.id 
-          }.length == 1 }.map { |v| [ v.id, v.variant_options_hash ] }]
-        [value.id.inspect, hsh]
-      }]]
-    }]
+  def variant_options_hash
+    return @_variant_options_hash if @_variant_options_hash
+    hsh = {}
+    variants.includes(:option_values).each do |variant|
+      variant.option_values.each do |ov|
+        hsh[ov.option_type_id] ||= {}
+        hsh[ov.option_type_id][ov.id] ||= {}
+        hsh[ov.option_type_id][ov.id][variant.id] = variant.variant_options_hash
+      end
+    end
+    @_variant_options_hash = hsh
   end
   
 end
