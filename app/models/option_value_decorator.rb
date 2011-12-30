783c456c3958a1cc2f4931e3d8bf1c5a093acabf
Spencer Steffen <spencer@citrusme.com>
2011-12-30 11:44:51 -0800

    remove check for spree_heroko in option value attachment definition

--- app/models/option_value_decorator.rb
+++ app/models/option_value_decorator.rb
@@ -2,20 +2,9 @@ OptionValue.class_eval do
 
   default_scope order(:position)
     
-  if defined?(SpreeHeroku)
-    has_attached_file :image, 
-      :styles => { :small => '40x30#', :large => '140x110#' },
-      :default_style => :small,
-      :path => "assets/option_values/:id/:style/:basename.:extension",
-      :storage => "s3",
-      :s3_credentials => "#{Rails.root}/config/s3.yml"
-  else
-    has_attached_file :image, 
-      :styles => { :small => '40x30#', :large => '140x110#' },
-      :default_style => :small,
-      :url => "/assets/option_values/:id/:style/:basename.:extension",
-      :path => ":rails_root/public/assets/option_values/:id/:style/:basename.:extension"
-  end
+  has_attached_file :image, 
+    :styles => { :small => '40x30#', :large => '140x110#' },
+    :default_style => :small
   
   def has_image?
     image_file_name && !image_file_name.empty?
