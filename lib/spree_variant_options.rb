d7c89f343a0a71f251d4558c1319caffcaf228ea
Spencer Steffen <spencer@citrusme.com>
2011-12-30 11:45:11 -0800

    fix decorator loader

--- lib/spree_variant_options.rb
+++ lib/spree_variant_options.rb
@@ -9,7 +9,7 @@ module SpreeVariantOptions
 
     config.to_prepare do
       #loads application's model / class decorators
-      Dir.glob File.expand_path("../../app/**/*_decorator*.rb") do |c|
+      Dir.glob File.expand_path("../../app/**/*_decorator.rb", __FILE__) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
