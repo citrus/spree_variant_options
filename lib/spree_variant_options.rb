7d397ebd1d6dd2c6af6414de7b33ddca85db4e08
Spencer Steffen <spencer@citrusme.com>
2012-02-27 09:41:15 -0800

    fix unintialized constant error from spree_auth and move engine to a seperate file

--- lib/spree_variant_options.rb
+++ lib/spree_variant_options.rb
@@ -1,33 +1,8 @@
-require 'spree_core'
-require 'spree_sample' unless Rails.env == 'production'
+require "spree_core"
+require "spree_sample" unless Rails.env.production?
 
-# This class is defined in spree_auth.. we'll define
-unless defined? Spree::User::DestroyWithOrdersError
-  module Spree
-    class User < ActiveRecord::Base
-      class DestroyWithOrdersError < StandardError; end
-    end
-  end
-end
+require "spree_variant_options/engine"
+require "spree_variant_options/version"
 
 module SpreeVariantOptions
-
-  class Engine < Rails::Engine
-
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    config.to_prepare do
-      #loads application's model / class decorators
-      Dir.glob File.expand_path("../../app/**/*_decorator.rb", __FILE__) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-
-      #loads application's deface view overrides
-      Dir.glob File.expand_path("../../app/overrides/*.rb", __FILE__) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
-    end
-    
-  end
-  
 end
