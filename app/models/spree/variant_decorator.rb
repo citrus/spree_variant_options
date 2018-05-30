cbf3e42b7d98c60e67f286587335ff955c8d990d
jayanti <jayanti@vinsol.com>
2018-05-30 16:23:55 +0530

    bug fixes

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,11 +1,15 @@
 Spree::Variant.class_eval do
-  validate :uniqueness_of_option_values,if: -> {!is_master}
+  validate :uniqueness_of_option_values
 
   def uniqueness_of_option_values
-    product.variants.find_each do |variant|
-      if variant.option_values.pluck('name').sort == option_values.collect(&:name).sort
+    product.variants.each do |v|
+      variant_option_values = v.option_values.ids
+      this_option_values = option_values.collect(&:id)
+      matches_with_another_variant = (variant_option_values.length == this_option_values.length) && (variant_option_values - this_option_values).empty?
+      if  !option_values.empty? &&  !(persisted? && v.id == id) && matches_with_another_variant
         errors.add(:base, :already_created)
       end
     end
   end
 end
+
